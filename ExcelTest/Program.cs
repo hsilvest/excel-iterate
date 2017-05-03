879f86af85d8630f3f4e7f8e5e07f22c1e2f24a8
Henrique Silvestre <henriquesilvestre@outlook.com>
2017-05-03 20:30:28 -0300

    initial commit

--- ExcelTest/Program.cs
+++ ExcelTest/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Spire.Pdf;
+using Spire.Xls.Converter;
+using System;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExcelTest
 {
@@ -10,6 +12,28 @@ namespace ExcelTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting process...");
+            var workbook = new XLWorkbook(@"teste.xlsx");
+
+            var dataSheet = workbook.Worksheet("Data");
+            var viewSheet = workbook.Worksheet("View");
+
+            var rnd = new Random();
+
+            for (int i = 1; i <= dataSheet.Rows().Count(); i++)
+            {
+                for (int j = 1; j <= 3; j++)
+                {
+                    var data = dataSheet.Row(i).Cell(j).Value;
+                    if (string.IsNullOrEmpty(data.ToString()))
+                        dataSheet.Row(i).Cell(j).Value = rnd.Next(10, 100);
+                }
+            }
+
+            workbook.SaveAs("sample.xlsx");
+
+            Console.WriteLine("Complete. Press any key to leave.");
+            Console.ReadLine();
         }
     }
 }
